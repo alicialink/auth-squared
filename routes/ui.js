95c82c25ebab8a80d31db349eaa5fa30a616da16
Alicia Key <alicia.key@galvanize.com>
2017-03-29 11:31:54 -0600

    X-101
    Awesome UI start

--- routes/ui.js
+++ routes/ui.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
   res.render('login', {})
 })
 
+router.get('/fail', (req, res) => {
+  res.render('fail', {})
+})
+
+router.get('/pass', (req, res) => {
+  res.render('pass', {})
+})
+
 module.exports = router

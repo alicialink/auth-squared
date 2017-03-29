88af62dbe650ddeb3c09a5b7f9eed936b1bf9bbf
Alicia Key <alicia.key@galvanize.com>
2017-03-29 11:24:49 -0600

    The first page is now rendered

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-console.log('hello')
+let express = require('express')
+let bodyParser = require('body-parser')
+let cookieSession = require('cookie-session')
+require('dotenv').config()
+
+let uiRoutes = require('./routes/ui.js')
+let app = express()
+
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.use('/', uiRoutes)
+app.use('/stylesheets', express.static('./public/stylesheets'))
+
+console.log('Starting...')
+let server = app.listen(3000, () => {
+  console.log(`Server listening on port ${server.address().port}`)
+})
 

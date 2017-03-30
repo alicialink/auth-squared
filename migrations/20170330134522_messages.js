c44b4bfb651aa686c4b6600cfa4fdac030ae7f51
Alicia Key <alicia.key@galvanize.com>
2017-03-30 13:53:34 -0600

    FK constraint

--- migrations/20170330134522_messages.js
+++ migrations/20170330134522_messages.js
@@ -1,7 +1,21 @@
+let tableName = 'messages'
+
 exports.up = function (knex, Promise) {
-  
+  return knex
+    .schema
+    .createTable(tableName, (tbl) => {
+      tbl.increments()
+      tbl
+        .string('message', 512)
+        .notNullable()
+      tbl
+        .integer('user_id')
+        .references('id')
+        .inTable('users')
+        .notNullable()
+    })
 }
 
 exports.down = function (knex, Promise) {
-  
+  return knex.schema.dropTableIfExists(tableName)
 }
